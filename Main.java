f39c6e7aa138c804990563a4d4caf0ed6cf5971c
Lalitha Srikumar <lalitha.sri-kumar@stud.hs-hannover.de>
2026-05-12 08:53:02 +0200

    Multiplikation wurde implementiert

--- Main.java
+++ Main.java
@@ -15,5 +15,11 @@ public class Main
         {
             summe = summe +i;
         }
+        
+        int mult = 1;
+        for(int i = 1; i <= 10; i++)
+        {
+            mult = mult *i;
+        }
     }
 }
