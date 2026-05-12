b0a1c8c06081a693427c1403e8fb394ce07aafca
Lalitha Srikumar <lalitha.sri-kumar@stud.hs-hannover.de>
2026-05-12 08:38:19 +0200

    Main wurde umgesetzt

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Main.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Main
+{
+    public static void main()
+    {
+        
+    }
+}
