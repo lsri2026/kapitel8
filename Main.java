c2a803ed2c9519c05bb76d1aa30ad1c43d32df12
Lalitha Srikumar <lalitha.sri-kumar@stud.hs-hannover.de>
2026-05-12 08:49:24 +0200

    Summe wurde implementiert

--- Main.java
+++ Main.java
@@ -9,6 +9,11 @@ public class Main
 {
     public static void main()
     {
+        int summe = 0;
         
+        for(int i = 0; i <= 20; i++)
+        {
+            summe = summe +i;
+        }
     }
 }
